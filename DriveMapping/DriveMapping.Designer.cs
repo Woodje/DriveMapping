c9740fe1d117b567e9ea81e42409739aea2d3439
Simon <simonjonpedersen@gmail.com>
2013-07-12 17:44:53 +0200

    DriveMapping
    A program made in a project at school.

---
+++ DriveMapping/DriveMapping.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace DriveMapping
+{
+    partial class DriveMapping
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DriveMapping));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(336, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 125);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(133, 171);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(133, 241);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(133, 274);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(195, 22);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "\\\\Computer\\";
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Brugermappe:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Kodeord:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Netværks-sti:";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "DriveMapping - Dobbeltklik for at åbne";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(336, 172);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 125);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Log af";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(36, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(432, 149);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Brugernavn:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(133, 205);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(195, 22);
+            this.textBox4.TabIndex = 1;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // DriveMapping
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 315);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "DriveMapping";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DriveMapping";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.DriveMapping_FormClosed);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.DriveMapping_KeyPress);
+            this.Resize += new System.EventHandler(this.DriveMapping_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ DriveMapping/DriveMapping.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace DriveMapping
+{
+    public partial class DriveMapping : Form
+    {
+        public DriveMapping()
+        {
+            InitializeComponent();
+        }
+
+        private void ok()
+        {
+            string Brugermappe = textBox1.Text, Kodeord = " " + textBox2.Text, Server = textBox3.Text, Brugernavn = @" /user:" + textBox4.Text;
+
+            if (textBox4.Text == "" && textBox2.Text != "")
+            {
+            }
+            else if (textBox4.Text != "" && textBox2.Text != "")
+            {
+                Process tid = Process.Start("net.exe", @"use K: " + Server + Brugermappe + Brugernavn + Kodeord + " /persistent:no");
+                tid.WaitForExit();
+                if (tid.ExitCode != 0)
+                {
+                    MessageBox.Show("Kunne ikke oprette forbindelse!", "Fejl i oprettelse!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    BringToFront();
+                }
+                else
+                {
+                    MessageBox.Show("Forbindelse oprettet til: " + Server + Brugermappe);
+                    button1.Visible = false;
+                    button2.Visible = true;
+                    textBox1.Enabled = false;
+                    textBox2.Enabled = false;
+                    textBox3.Enabled = false;
+                    textBox4.Enabled = false;
+                    WindowState = FormWindowState.Minimized;
+                }
+            }
+            else if (textBox4.Text != "" && textBox2.Text == "")
+            {
+                Process tid = Process.Start("net.exe", @"use K: " + Server + Brugermappe + Brugernavn + " /persistent:no");
+                tid.WaitForExit();
+                if (tid.ExitCode != 0)
+                {
+                    MessageBox.Show("Kunne ikke oprette forbindelse!", "Fejl i oprettelse!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    BringToFront();
+                }
+                else
+                {
+                    MessageBox.Show("Forbindelse oprettet til: " + Server + Brugermappe);
+                    button1.Visible = false;
+                    button2.Visible = true;
+                    textBox1.Enabled = false;
+                    textBox2.Enabled = false;
+                    textBox3.Enabled = false;
+                    textBox4.Enabled = false;
+                    WindowState = FormWindowState.Minimized;
+                }
+            }
+            else if (textBox4.Text == "" && textBox2.Text == "")
+            {
+                Process tid = Process.Start("net.exe", @"use K: " + Server + Brugermappe + " /persistent:no");
+                tid.WaitForExit();
+                if (tid.ExitCode != 0)
+                {
+                    MessageBox.Show("Kunne ikke oprette forbindelse!", "Fejl i oprettelse!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    BringToFront();
+                }
+                else
+                {
+                    MessageBox.Show("Forbindelse oprettet til: " + Server + Brugermappe);
+                    button1.Visible = false;
+                    button2.Visible = true;
+                    textBox1.Enabled = false;
+                    textBox2.Enabled = false;
+                    textBox3.Enabled = false;
+                    textBox4.Enabled = false;
+                    WindowState = FormWindowState.Minimized;
+                }
+            }
+        }
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void DriveMapping_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.BalloonTipTitle = "DriveMapping";
+                notifyIcon1.BalloonTipText = "Dobbeltklik for at åbne";
+                notifyIcon1.ShowBalloonTip(3000);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string Bruger = textBox1.Text, Kodeord = textBox2.Text, Server = textBox3.Text;
+
+            Process tid = Process.Start("net.exe", @"use K: /d");
+            tid.WaitForExit();
+            if (tid.ExitCode == 0)
+            {
+                MessageBox.Show("Forbindelse til: " + Server + Bruger + " Afbrudt!");
+                Close();
+            }
+        }
+
+        private void DriveMapping_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (textBox1.Enabled == false)
+            {
+                string Bruger = textBox1.Text, Kodeord = textBox2.Text, Server = textBox3.Text;
+
+                Process tid = Process.Start("net.exe", @"use K: /d");
+                tid.WaitForExit();
+                if (tid.ExitCode == 0)
+                {
+                    MessageBox.Show("Forbindelse til: " + Server + Bruger + " Afbrudt!");
+                    Close();
+                }
+            }
+        }
+
+        private void DriveMapping_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                ok();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ok();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                ok();
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                ok();
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                ok();
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                ok();
+            }
+        }
+    }
+}
